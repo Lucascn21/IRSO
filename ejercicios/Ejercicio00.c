875914561e9cab23777b7cf73a92e5d074b90e7e
LGM <lucascn21@gmail.com>
2019-08-14 18:17:44 -0300

    Primer ejerccio en C.
     Se printean asteriscos, dejando espacios.

---
+++ ejercicios/Ejercicio00.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	char caracter = '*';
+	printf("%c\n",caracter);
+	printf("%2c\n",caracter);
+	printf("%3c\n",caracter);
+	printf("%4c\n",caracter);
+	printf("%c\n",caracter);
+	
+	system("Pause");
+	return 0;
+}
