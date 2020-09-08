84280f81c34818e3e32d01005e4a790e1e878c31
Lucascn21 <lucascn21@gmail.com>
2020-09-08 17:13:07 -0300

    Ejercicios 1-2 de la serie B

---
+++ Estructura_2c/ejercicios/B_ejercicio1.cpp
@@ -0,0 +1,43 @@
+/*
+Un procedimiento que lea un entero positivo validado. 
+Leer varios positivos y mostrar cuantos de ello fueran pares. 
+Para ello hacer una función que  reciba un entero y devuelva 1  si el entero fuera par y 0 en caso contrario.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void contarPPares(int &cont);
+int esPositivo(int a);
+
+
+int main(){
+	int contPositivos=0;
+	contarPPares(contPositivos);
+	contarPPares(contPositivos);
+	printf("total de positivos pares %d",contPositivos);
+	return 0;
+}
+
+
+//Procedimiento que 
+void contarPPares(int &cont){ 
+	int num;
+	printf("Dame un numero entero positivo ");
+   	scanf("%d", &num);
+   	fflush(stdin);
+	if(esPositivo(num)==1&&num%2==0)cont++;
+	else printf("No es un numero entero, positivo y/o par\n");
+}
+
+
+int esPositivo(int a){
+	if(a>0){
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+
+
---
+++ Estructura_2c/ejercicios/B_ejercicio2.cpp
@@ -0,0 +1,42 @@
+/*
+Un procedimiento que lea un nro impar. 
+Leer varios pares de enteros  impares y por cada par mostrar una leyenda aclaratoria dependiendo de cual fuera el mayor.
+ Para ello hacer  una función que reciba dos enteros y devuelva el mayor de ambos. 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void pedirImpar(int &num);
+int determinarMayor(int a, int b);
+
+int main(){
+	int num=0, num2=0;
+	pedirImpar(num);
+	pedirImpar(num2);
+	if(num!=num2)printf("el mayor es %d\n",determinarMayor(num, num2));
+	pedirImpar(num);
+	pedirImpar(num2);
+	if(num!=num2)printf("el mayor es %d",determinarMayor(num, num2));
+	else printf("Son iguales");
+	return 0;
+}
+
+void pedirImpar(int &num){
+	printf("Dame un numero entero impar ");
+   	scanf("%d", &num);
+   	fflush(stdin);
+	while(num%2==0){
+	printf("No era un impar, intenta otra vez: ");
+   	scanf("%d", &num);
+   	fflush(stdin);
+	}
+	
+}
+
+int determinarMayor(int a, int b){
+	if(a>b)return a;
+	else return b;
+}
+
+
+
